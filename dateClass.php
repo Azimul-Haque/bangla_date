cf3d24a530f3c3725cdee9903c7153c89f6121f3
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-17 22:46:27 +0600

    sad324

--- dateClass.php
+++ dateClass.php
@@ -7,37 +7,7 @@
  * @copyright 2020 A. H. M. Azimul Haque
  *
  *
- *
- * Get Output
- * $output = $bn->get_date();
- * print_r($output);
  */
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-// testing
-header('content-type: application/json; charset=utf-8');
-
-$mydate = (object) null;
-$mydate->en_date = date('Y-m-d H:i:s');
-$mydate->bn_date = bangla_date(date('Y-m-d H:i:s'));
-
-$myJSON = json_encode($mydate);
-
-echo $myJSON;
-
-function bangla_date($date) {
-    $bn = new BanglaDate(strtotime($date));
-
-    $output = $bn->get_date();
-
-    return $output[1] . ' ' . $output[0]  . ', ' . $output[2] . ' বঙ্গাব্দ';
-}
-// testing
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
 
 class BanglaDate {
     private $timestamp; //timestamp as input
